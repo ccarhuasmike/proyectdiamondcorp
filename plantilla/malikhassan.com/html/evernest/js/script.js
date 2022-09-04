000c4084c79dc8bc16fafb9f75803ca8d6cca089
mike.ccarhuas <mike.ccarhuas@efitec.pe>
2022-09-04 14:00:57 -0500

    [update] modificacion de datos

--- plantilla/malikhassan.com/html/evernest/js/script.js
+++ plantilla/malikhassan.com/html/evernest/js/script.js
@@ -76,4 +76,22 @@ $('[data-toggle="offcanvas"]').on('click', function () {
   });
 
 
+  $('.carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '.slider'
+  });
+ 
+  $('.slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: ".carousel"
+  });
+
 })(jQuery);;
